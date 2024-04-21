e8530ec57355797dd4013bbaed4e7d7a65b6e32d
Muhamad Jamaludin <muhhjam@gmail.com>
2024-04-21 23:46:32 +0700

    update level-2

--- src/views/TweetView.vue
+++ src/views/TweetView.vue
@@ -7,7 +7,7 @@
 				} cursor-pointer text-[16px] uppercase`">FOR YOU</span>
 			<span @click="isFollowing = true"  :class="`${isFollowing === true ?
 					'underline decoration-blue-600 underline-offset-4 decoration-4 cursor-pointer': ''
-				} cursor-pointer text-[16px] uppercase`">FOLLOWING </span>
+				} cursor-pointer text-[16px] uppercase`">FOLLOWING</span>
 		</div>
 	</nav>
 	<div class="flex flex-col justify-center">
@@ -26,7 +26,7 @@
 				@getData="getTweet"
 			/>
 		</div>
-		<div v-else class="flex justify-center bg-white px-5 py-[24px] text-[#999999] font-semibold text-[24px] items-center">
+		<div v-else class="flex justify-center bg-white px-5 py-[24px] text-[#999999] font-semibold text-[24px] items-center uppercase">
 		NO TWEET
 	</div>
 	</div>
